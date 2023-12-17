0eabb812882107a593834d3105a7fac70314f7ae
ryanbakker <rmbakker2002@gmail.com>
2023-12-17 23:40:14 +1300

    Event Detail Page

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -5,6 +5,17 @@ import { handleError } from "../utils";
 import { connectToDatabase } from "../database";
 import User from "../database/models/user.model";
 import Event from "../database/models/event.model";
+import Category from "../database/models/cateogry.model";
+
+const populateEvent = async (query: any) => {
+  return query
+    .populate({
+      path: "organizer",
+      model: User,
+      select: "_id firstName lastName",
+    })
+    .populate({ path: "category", model: Category, select: "_id name" });
+};
 
 export const createEvent = async ({
   event,
@@ -27,6 +38,22 @@ export const createEvent = async ({
     });
 
     return JSON.parse(JSON.stringify(newEvent));
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const getEventById = async (eventId: string) => {
+  try {
+    await connectToDatabase();
+
+    const event = await populateEvent(Event.findById(eventId));
+
+    if (!event) {
+      throw new Error("Event not found!");
+    }
+
+    return JSON.parse(JSON.stringify(event));
   } catch (error) {
     handleError(error);
   }
