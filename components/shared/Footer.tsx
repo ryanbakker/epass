7c0e1376f070dfe98ca1720118a92e3a4e662a2f
ryanbakker <rmbakker2002@gmail.com>
2023-12-17 19:04:12 +1300

    Add Clerk User to Database

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <div>
-      <h2>Footer</h2>
-    </div>
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+
+        <p className="text-sm text-gray-500">
+          2023 ePass. All Rights Reserved.
+        </p>
+      </div>
+    </footer>
   );
 };
 
