b95a377b16659a947b8088dd0342686454b795d2
ryanbakker <rmbakker2002@gmail.com>
2023-12-19 16:25:06 +1300

    Pagination

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -60,7 +60,7 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
           emptyTitle="No event have been created yet"
           emptyStateSubtext="Go create some now"
           collectionType="Events_Organized"
-          limit={6}
+          limit={3}
           page={eventsPage}
           urlParamName="eventsPage"
           totalPages={organizedEvents?.totalPages}
